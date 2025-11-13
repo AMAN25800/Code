c55556ca139091d1c877dbc73eb1937be16b0e0c
Aman Shah <166643927+AMAN25800@users.noreply.github.com>
2025-11-13 16:16:10 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 # Start the backend executable on internal port 5000
 /app/jiotv_go-linux-amd64 &
 
-# Wait until port 5000 is open
+# Wait for port 5000 to be open
 echo "Waiting for backend to start on port 5000..."
 while ! nc -z localhost 5000; do
   sleep 1
 done
 
-# Replace ${PORT} in NGINX template with Render's environment variable
+# Substitute $PORT in NGINX template
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start NGINX in foreground
