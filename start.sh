bd70255ab85805cb76d7607383963661e8d6981a
Aman Shah <166643927+AMAN25800@users.noreply.github.com>
2025-11-13 21:18:23 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
+# start.sh
 #!/bin/bash
 set -e
 
-# Start JioTV Go on localhost:5002
+# Start JioTV Go
 ./jiotv_go-linux-amd64 serve --host 0.0.0.0 --port 5002 &
-JTV_PID=$!
 
-# Configure ngrok with auth token from environment
-ngrok authtoken $NGROK_AUTH_TOKEN
+# Authenticate ngrok
+ngrok config add-authtoken $NGROK_AUTH_TOKEN
 
 # Start ngrok HTTP tunnel
-ngrok http 5002 --log=stdout &
-NGROK_PID=$!
-
-# Wait for any process to exit
-wait -n
-
-# Kill all processes if one exits
-kill $JTV_PID $NGROK_PID
+ngrok tunnel http 5002
