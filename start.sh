a61fa7e0467c9ea2f48b5d3d3fdf7593fde83683
Aman Shah <166643927+AMAN25800@users.noreply.github.com>
2025-11-13 21:51:46 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-# Start JioTV Go
+# Start JioTV Go in background
 ./jiotv_go-linux-amd64 serve --host 0.0.0.0 --port 5002 &
 
-# Start ngrok with authtoken from environment variable
-ngrok config add-authtoken $NGROK_AUTHTOKEN
+# Export ngrok token from environment
+export NGROK_AUTHTOKEN=$NGROK_AUTHTOKEN
+
+# Start ngrok tunnel
 ngrok http 5002 --log=stdout
